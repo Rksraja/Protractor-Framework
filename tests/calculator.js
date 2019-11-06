7b36ec31ceabca4b87fa40443e0e9cd783099c4b
Rksraja <rksraja.143@gmail.com>
2019-11-06 12:23:29 +0530

    changes in cal

--- tests/calculator.js
+++ tests/calculator.js
@@ -23,25 +23,25 @@ describe('demo calculator tests', function () {
 
 });
 
-it('subtraction test', function () {
+// it('subtraction test', function () {
 
 
-    // browser.get('http://juliemr.github.io/protractor-demo/');
-    Homepage.get('http://juliemr.github.io/protractor-demo/');
-    //element(by.model('first')).sendKeys('3');      
-    Homepage.enterFirstNumber('3');
+//     // browser.get('http://juliemr.github.io/protractor-demo/');
+//     Homepage.get('http://juliemr.github.io/protractor-demo/');
+//     //element(by.model('first')).sendKeys('3');      
+//     Homepage.enterFirstNumber('3');
 
-   // element(by.model('second')).sendKeys('13');  
-    Homepage.enterSecondNumber('13');
+//    // element(by.model('second')).sendKeys('13');  
+//     Homepage.enterSecondNumber('13');
 
-    //element(by.css('[ng-click="doAddition()"]')).click();
-    Homepage.clickGo1();
+//     //element(by.css('[ng-click="doAddition()"]')).click();
+//     Homepage.clickGo1();
 
-    let result=element(by.cssContainingText('.ng-binding','16'));
-    expect(result.getText()).toEqual('16');
-    Homepage.VerifyResult('16');
-    browser.sleep(3000);
+//     let result=element(by.cssContainingText('.ng-binding','16'));
+//     expect(result.getText()).toEqual('16');
+//     Homepage.VerifyResult('16');
+//     browser.sleep(3000);
 
 
-});
+// });
 });
